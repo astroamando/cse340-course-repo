310a09a8e24523f63562caf39164fed0767f568a
astroamando <amando.filho83@gmail.com>
2026-08-15 01:48:47 -0600

    Finalize W05 authorization messages

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,6 +1,8 @@
 const requireLogin = (req, res, next) => {
   if (!req.session.user) {
-    return res.redirect("/login");
+    return res.redirect(
+      "/login?notice=Please log in to access that page."
+    );
   }
 
   next();
@@ -9,11 +11,15 @@ const requireLogin = (req, res, next) => {
 const requireRole = (role) => {
   return (req, res, next) => {
     if (!req.session.user) {
-      return res.redirect("/login");
+      return res.redirect(
+        "/login?notice=Please log in to access that page."
+      );
     }
 
     if (req.session.user.role !== role) {
-      return res.redirect("/dashboard");
+      return res.redirect(
+        "/dashboard?notice=You do not have permission to access that page."
+      );
     }
 
     next();
